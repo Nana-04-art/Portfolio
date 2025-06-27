58cc4184f7e8b462566ae469e9194f172805ba25
Nana-04-art <natalia.busuioc@itsrizzoli.it>
2025-06-27 12:38:37 +0200

    Initial commit

---
+++ src/HomePage.jsx
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './assets/style/palette.css';
+import './HomePage.css'
+
+function HomePage() {
+
+    return (
+        <div className="HomePage">
+            <header><h1>Portfolio di Nyx2112</h1></header>
+
+
+        </div>
+    )
+}
+
+export default HomePage
