3bd969ebe18d3c238fe2a3f4301ff2022afda310
Nana-04-art <natalia.busuioc@itsrizzoli.it>
2025-06-27 23:20:28 +0200

    miglioramento css con aggiunta di background ai testi, manca la'allineamneto al centro + creazione componente chiamato "Progetti"

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -12,38 +12,60 @@ function HomePage() {
                 <header>
                     <h1>Portfolio di Nyx2112</h1>
                     <nav>
-                        <button id="home" >Home</button>
+                        <button id="home">Home</button>
                         <button id="progetti">Progetti</button>
                         <button id="design">Design</button>
                     </nav>
                 </header>
             </div>
             <div className="main">
-                <h1>About Me</h1>
-                <p>Ciao! Mi chiamo Natalia e sono una persona intraprendente, curiosa e creativa. Attualmente sto
-                    frequentando il corso Software Architect Specialist e sono particolarmente attratta dal mondo dello
-                    sviluppo front-end. Allo stesso tempo, nutro un forte interesse per l’editoria e la scrittura, che
-                    porto con me come passione.
+                <h1>About Me:</h1>
+                <p>Ciao! Mi chiamo Natalia e sono una persona intraprendente, curiosa e creativa.
+                    Attualmente sto
+                    frequentando il corso Software Architect Specialist e sono particolarmente attratta
+                    dal mondo dello sviluppo front-end. Allo stesso tempo, nutro un forte interesse per l’editoria e la
+                    scrittura,
+                    che porto con me come passione.<br/>
                     Sono aperta a diverse opportunità professionali, guidata dalla voglia di mettermi alla prova,
-                    imparare continuamente e crescere nel settore informatico.
-                    Ho creato questo sito per condividere alcuni dei progetti che ho realizzato durante il corso. In
-                    alto a sinistra troverai tre pulsanti: Home, Progetti e Design — ti invito a esplorare e divertirti!
+                    imparare continuamente e crescere nel settore informatico.<br/>
+                    Ho creato questo sito per condividere alcuni dei progetti che ho realizzato durante il corso. <br/>
+                    Non so se hai notato ma se guardi in alto a sinistra troverai tre pulsanti: Home, Progetti e Design
+                    — ti invito a esplorare e divertirti!<br/>
                     Se vuoi scoprire di più su di me, dai un’occhiata alla lista qui sotto.
                 </p>
             </div>
             <div className="lista">
-                <ul>
-                    <p>Sono appassionata di:</p>
-                    <li>Scrittura</li>
-                    <li>Lettura e Letteratura,</li>
-                    <li>Gastronomia</li>
-                    <li>Viaggi</li>
-                    <li>Storia</li>
-                    <p>Sono una scrittrice:</p>
-                    <li> Attualmente sto scrivendo un libro urban fantasy incentrato su due temi: il contrasto tra il bene e il male, mai netto ma sfumato, e la trasformazione e l'evoluzione personale.</li>
-                    <p>Faccio volontariato:</p>
-                    <li>donando il sangue</li>
-                </ul>
+                <h2>Altre cose su di me:</h2>
+
+                <div className="ul-container">
+                    <div>
+                        <ul className="passione">
+                            <p>Sono appassionata di:</p>
+                            <li>Scrittura</li>
+                            <li>Lettura</li>
+                            <li>Gastronomia</li>
+                            <li>Viaggi</li>
+                            <li>Storia</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <ul className="scrittrice">
+                            <p>Sono una scrittrice:</p>
+                            <li>Sto scrivendo un libro urban fantasy incentrato su due temi: il contrasto tra il bene e
+                                il male, mai netto ma sfumato, e la trasformazione e l'evoluzione personale.
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div>
+
+                        <ul className="volontariato">
+                            <p>Faccio volontariato:</p>
+                            <li>Dono il sangue</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
 
 
